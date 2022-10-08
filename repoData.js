2f2447f5e6eef8288a5a1e9b242683e1f54f4bd9
Nebula <me@itsnebula.net>
2022-10-08 14:24:10 -0400

    add emojis

--- repoData.js
+++ repoData.js
@@ -1,8 +1,8 @@
 //Add Name of your Repo here
-exports.name = `Example Repo`;
+exports.name = `Nebula's Repo`;
 //Add Name and Type of Image Icon you want to use here
-exports.icon = 'CoolIcon.png';
+exports.icon = 'icon.png';
 //Add the folder name where all your emotes are saved
 exports.pathName = 'emotes';
 //Add your name here, or remove the line if you don't need your name displayed under the repo's title in our clients
-exports.author = 'Joe';
+exports.author = 'Nebula';
