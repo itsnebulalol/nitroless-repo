2c45a6b99c021b8cd7a0fef6035d1d4e1f9f456a
Lakhan Lothiyi <42747613+llsc12@users.noreply.github.com>
2022-10-10 00:38:30 +0100

    Update repoData.js

--- repoData.js
+++ repoData.js
@@ -1,7 +1,7 @@
 //Add Name of your Repo here
 exports.name = `Nebula's Repo`;
 //Add Name and Type of Image Icon you want to use here
-exports.icon = 'icon.png';
+exports.icon = 'kirbi.gif';
 //Add the folder name where all your emotes are saved
 exports.pathName = 'emotes';
 //Add your name here, or remove the line if you don't need your name displayed under the repo's title in our clients
